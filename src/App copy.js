8587fea51ed171b1a5300c6f4e883b0830902b45
moreiranat <natalylucena.pb@gmail.com>
2022-05-10 01:03:04 -0300

    Merge branch 'master' of https://github.com/moreiranat/projeto-frontend-com-react

---
+++ src/App copy.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './App.css';
+
+export default class App extends React.Component {
+
+state = {
+  name: ""
+}
+
+create = () => {
+  console.log("Nome da Categoria: " + this.state.name);
+}
+
+  render() {
+    return (
+
+      <div className="container">
+        
+        <div className='row'>
+          <div className='col-6 offset-md-3'>
+            <h1 class="display-3 text-center">Salvar Categoria</h1>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-6 offset-md-3">
+            
+            <form action='' className='form'>
+              <div>
+                <label className='form-label'>Nome da Categoria</label>
+                <input className='form-control' type="text" value={this.state.name} onChange={(e) => {this.setState({name: e.target.value})}} />
+              </div>
+
+              <br/>
+              <input className="form-control btn btn-primary " type="button" value="Salvar" onClick={this.create}/>
+              
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  } 
+} 
