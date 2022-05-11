12b260a9c2935fe2d2d7bb4381cef8df4ccad969
moreiranat <natalylucena.pb@gmail.com>
2022-05-11 14:42:11 -0300

    Update tela Salvar Categoria

--- src/App copy.js
+++ src/App copy.js
@@ -3,21 +3,22 @@ import './App.css';
 
 export default class App extends React.Component {
 
-state = {
-  description: "",
-  priority: "",
-  categoryName: ""
-}
+  state = {
+    description: "",
+    priority: "",
+    categoryName: ""
+  }
 
-create = () => {
-  alert("A tarefa -" + this.state.description + "- foi salva!");
-}
+  create = () => {
+ 
+    // alert("A tarefa -" + this.state.description + "- foi salva!");
+  }
 
   render() {
     return (
 
       <div className="container">
-        
+
         <div className='row'>
           <div className='col-6 offset-md-3'>
             <h1 class="display-3 text-center">Salvar Tarefa</h1>
@@ -26,28 +27,28 @@ create = () => {
 
         <div className="row">
           <div className="col-6 offset-md-3">
-            
+
             <form action='' className='form'>
               <div>
                 <label className='form-label'>Descrição: </label>
-                <input className='form-control' type="text" value={this.state.description} onChange={(e) => {this.setState({description: e.target.value})}} />
-                
-                <br/>
+                <input className='form-control' type="text" value={this.state.description} onChange={(e) => { this.setState({ description: e.target.value }) }} />
+
+                <br />
                 <label className='form-label'>Prioridade: </label>
-                <input className='form-control' type="text" value={this.state.priority} onChange={(e) => {this.setState({priority: e.target.value})}} />
-                
-                <br/>
-                <label className='form-label'>Nome da Categoria: </label>              
-                <input className='form-control' type="text" value={this.state.categoryName} onChange={(e) => {this.setState({categoryName: e.target.value})}} />
+                <input className='form-control' type="text" value={this.state.priority} onChange={(e) => { this.setState({ priority: e.target.value }) }} />
+
+                <br />
+                <label className='form-label'>Nome da Categoria: </label>
+                <input className='form-control' type="text" value={this.state.categoryName} onChange={(e) => { this.setState({ categoryName: e.target.value }) }} />
               </div>
 
-              <br/>
-              <input className="form-control btn btn-primary " type="button" value="Salvar" onClick={this.create}/>
-              
+              <br />
+              <input className="form-control btn btn-primary " type="button" value="Salvar" onClick={this.create} />
+
             </form>
           </div>
         </div>
       </div>
     );
-  } 
+  }
 } 
--- src/App.js
+++ src/App.js
@@ -3,41 +3,39 @@ import './App.css';
 
 export default class App extends React.Component {
 
-state = {
-  name: ""
-}
+  state = {
+    name: ""
+  }
 
-create = () => {
-  alert("A categoria -" + this.state.name + "- foi salva!");;
-}
+  create = () => {
+    alert("A categoria -" + this.state.name + "- foi salva!");
+  }
 
   render() {
     return (
 
       <div className="container">
-        
+
         <div className='row'>
           <div className='col-6 offset-md-3'>
-            <h1 class="display-3 text-center">Salvar Categoria</h1>
+            <h1 class="display-3 text-center fs-2 fw-bold fst-italic">Salvar Categoria</h1>         
           </div>
         </div>
 
+        <br />
+
         <div className="row">
           <div className="col-6 offset-md-3">
-            
-            <form action='' className='form'>
-              <div>
-                <label className='form-label'>Nome da Categoria:</label>
-                <input className='form-control' type="text" value={this.state.name} onChange={(e) => {this.setState({name: e.target.value})}} />
-              </div>
 
-              <br/>
-              <input className="form-control btn btn-primary " type="button" value="Salvar" onClick={this.create}/>
-              
+            <form action='' className='form'>
+              <div className="input-group mb-3">
+                <input className='form-control' type="text" placeholder="Nome da Categoria" aria-label="Nome da Categoria" aria-describedby="button-addon2" value={this.state.name} onChange={(e) => { this.setState({ name: e.target.value }) }} />
+                <input className="btn btn-outline-secondary" type="button" id="button-addon2" value="Salvar" onClick={this.create} />
+              </div>
             </form>
           </div>
         </div>
       </div>
     );
-  } 
+  }
 } 
